3c0aa545d9949a91f3426fefdaaa3a2075a45476
djirdehh <hassan.djirdeh@gmail.com>
2024-08-06 11:18:10 -0400

    update

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -299,7 +299,7 @@ export default function Home() {
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
                     <span>
-                      4 live online lectures on Wednesdays at 7:30 PM ET (first
+                      4 live online lectures on Wednesdays at 7:00 PM ET (first
                       lecture on Sept 25)
                     </span>
                   </li>
